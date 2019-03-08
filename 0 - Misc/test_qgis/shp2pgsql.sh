b8f786f1de3c56c4585366dc0e91bea0c853a440
Basel Shbita <basel921@gmail.com>
2019-03-08 13:55:37 -0800

    remove unused files

--- 0 - Misc/test_qgis/shp2pgsql.sh
+++
@@ -1,8 +0,0 @@
-shp2pgsql -I -s 4326 test_line.shp public.test_line | psql -U postgres -d test_qgis
-shp2pgsql -I -s 4326 altered.shp public.altered | psql -U postgres -d test_qgis
-# -d: database name
-# public.test_line: table name
-# -s: srid, Spatial Reference System Identifier
-# -I: Create a GiST index on the geometry column
-
-# http://www.bostongis.com/pgsql2shp_shp2pgsql_quickguide.bqg
