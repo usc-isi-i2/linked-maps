4165776a56d855dc072c7b5f9e49bb679f1f3f65
Basel Shbita <basel921@gmail.com>
2019-03-11 15:58:40 -0700

    update flask with an example

--- app/main.py
+++ app/main.py
@@ -4,7 +4,9 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template('index.html')
+    data = ['LINESTRING(34.79011521585437 32.11589598470496,34.78736863382312 32.06470337558891)', \
+            'LINESTRING(34.93019089944812 32.20192792321161,35.13618455179187 32.16705989986249)']
+    return render_template('index.html', data = data)
 
 if __name__ == '__main__':
     app.run(host="localhost", port=5000, debug=True)
