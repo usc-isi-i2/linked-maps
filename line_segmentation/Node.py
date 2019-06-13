36fb69d51b5c4f4d9ac5815e1dedfeab1cf43fdc
Patavee Meemeng <patavee@avata.ai>
2019-06-13 14:27:20 -0700

    finish intersect method

--- line_segmentation/Node.py
+++ line_segmentation/Node.py
@@ -10,15 +10,81 @@ class Node:
         self.metadata = metadata
         self.gid = gid
         self.name = name
+        self.parent = dict()  # parentname : node object
+        self.child = dict()  # childname : node object
 
-    def intersect(self, other_node):
-        pass
+    def __repr__(self):
+        return "name : {}, parent: {}, child: {}".format(self.name, self.parent.keys(), self.child.keys())
+
+    def intersect(self, other_node, new_name):
+        if self.metadata != other_node.metadata:
+            print("ERROR: Metadata Mismatched")
+            return None
+
+        if self == other_node:
+            print("ERROR: Same Node")
+            return None
+
+        if self.name == other_node.name:
+            print("ERROR: Same Name")
+            return None
+
+        cur = self.metadata.connection.cursor()
+
+
+        SQL = '''
+            INSERT INTO %s (wkt, geom) 
+            SELECT ST_ASTEXT(res.lr), lr
+            FROM(
+                SELECT ST_MULTI(ST_INTERSECTION(
+                    l.geom, 
+                    ST_INTERSECTION(
+                        st_buffer(l.geom, 0.0015), 
+                        st_buffer(r.geom, 0.0015))         
+                )) as lr
+                FROM 
+                    (
+                    SELECT geom
+                    FROM %s
+                    WHERE %s.gid = %s
+                    ) as l,
+                    (
+                    SELECT geom
+                    FROM %s
+                    WHERE %s.gid = %s
+                    ) as r
+            ) res
+            where ST_geometrytype(res.lr) = 'ST_MultiLineString'
+            RETURNING gid
+
+        '''
+        cur.execute(SQL,
+                    (AsIs(self.metadata.table_name),
+                     AsIs(self.metadata.table_name),
+                     AsIs(self.metadata.table_name),
+                     AsIs(self.gid),
+                     AsIs(self.metadata.table_name),
+                     AsIs(self.metadata.table_name),
+                     AsIs(other_node.gid),
+                     ))
+
+        new_gid = cur.fetchone()
+        new_node = Node(self.metadata, new_gid, new_name)
+
+        self.child[new_name] = new_node
+        other_node.child[new_name] = new_node
+        new_node.parent[self.name] = self
+        new_node.parent[other_node.name] = other_node
+
+        return new_node
+
 
     def union(self, other_node):
         pass
 
     def minus(self, other_node):
         pass
+
 
     @classmethod
     def from_shapefile(cls, path, metadata, name):
@@ -94,7 +160,15 @@ class Metadata:
 
 if __name__ == "__main__":
     # test
+    
     config_path = r"C:\Users\Wii\Google Drive USC\usc\ISI\7maps_clip\clipped\config2.json"
     meta = Metadata(config_path)
 
-    node = Node.from_shapefile(r"C:\Users\Wii\Google Drive USC\usc\ISI\7maps_clip\clipped\1954c.shp", meta, "1954")
+    node54 = Node.from_shapefile(r"C:\Users\Wii\Google Drive USC\usc\ISI\7maps_clip\clipped\1954c.shp", meta, "1954")
+    node62 = Node.from_shapefile(r"C:\Users\Wii\Google Drive USC\usc\ISI\7maps_clip\clipped\1962c.shp", meta, "1962")
+
+    new_node = node54.intersect(node62, "1954-1962")
+    print("node 1954", node54)
+    print("node 1962", node54)
+    print("new node", new_node)
+
