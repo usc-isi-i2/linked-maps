27e00f20d3d7a78cd62f5bdd72b122c682046bb3
jainvasu631 <vasujain@D20ML-VJAIN.local>
2019-06-27 14:00:44 -0700

    configured geonames and coordinate parser for csv

--- geonames/geonames.py
+++ geonames/geonames.py
@@ -1,22 +1,25 @@
 import urllib
 import json
 import random
-from parse_coordinates import openCoordinates,parseCoordinates
+from parse_coordinates import openGeom,parseCoordinates
 geonames_service = {
     'findNearby':"http://api.geonames.org/findNearbyJSON?",
     'findNearbyPlaceName':"http://api.geonames.org/findNearbyPlaceNameJSON?",
     'findNearbyWikipedia':"http://api.geonames.org/findNearbyWikipediaJSON?"
     }
-coordinates = openCoordinates("coordinates")
-def generateRandomCoordinates(north=90,south=-90,east=180,west=-180, N=10):
-    coordinates = [{'lat':random.uniform(north,south),'lng':random.uniform(west,east)} for i in range(N)]
+def generateRandomCoordinate(north=90.0,south=-90.0,east=180.0,west=-180.0):
+    coordinate = {'lat':random.uniform(north,south),'lng':random.uniform(west,east)}
+    return coordinate
+
+def generateRandomCoordinates(N=10,north=90.0,south=-90.0,east=180.0,west=-180.0):
+    coordinates = [ generateRandomCoordinate(north,south,east,west) for _ in range(N)]
     return coordinates
     
 def getGeonameData(coordinate = {'lat':0,'lng':0}, service = 'findNearby'):    
     username = "jainvasu631"
     # http://api.geonames.org/findNearbyJSON?lat=47.3&lng=19&username=jainvasu631
     url_base = geonames_service['findNearbyWikipedia']
-    url_params = "lat="+coordinate['lat']+"&lng="+coordinate['lng']+"&username="+username
+    url_params = "lat="+str(coordinate['lat'])+"&lng="+str(coordinate['lng'])+"&username="+username
     url = url_base + url_params
     response = urllib.urlopen(url)
     geoname_data = json.loads(response.read())
@@ -25,3 +28,8 @@ def getGeonameData(coordinate = {'lat':0,'lng':0}, service = 'findNearby'):
 def getGeonamesData(coordinates,service = 'findNearby' ):
     geoname_data = [getGeonameData(coordinate,service) for coordinate in coordinates]
     return geoname_data
+
+segment1 = openGeom("geom.csv")[1]
+print segment1
+print len(segment1['wkt'])
+#print getGeonamesData(segment1['wkt'])
