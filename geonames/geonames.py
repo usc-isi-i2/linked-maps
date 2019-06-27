70db36c591b9feae8cba4bbcdd94163cbee5b8d9
jainvasu631 <vasujain@D20ML-VJAIN.local>
2019-06-27 11:34:31 -0700

    location_ref to store coordinates name and data.

--- geonames/geonames.py
+++ geonames/geonames.py
@@ -1,12 +1,17 @@
 import urllib
 import json
-(lat,lng) = ('0','0')
-username = "jainvasu631"
-# http://api.geonames.org/findNearbyJSON?lat=47.3&lng=19&username=jainvasu631
-url_base = "http://api.geonames.org/findNearbyJSON?"
-url_params = "lat="+lat+"&lng="+lng+"&username="+username
-url = url_base + url_params
-print(url)
-response = urllib.urlopen(url)
-result = json.loads(response.read())
-print(result) 
+import random
+location_ref = {}
+for i in range(1):
+    lat = str(random.uniform(-90,90))
+    lng = str(random.uniform(-90,90))
+    coordinates = (lat,lng)
+    username = "jainvasu631"
+    # http://api.geonames.org/findNearbyJSON?lat=47.3&lng=19&username=jainvasu631
+    url_base = "http://api.geonames.org/findNearbyJSON?"
+    url_params = "lat="+lat+"&lng="+lng+"&username="+username
+    url = url_base + url_params
+    response = urllib.urlopen(url)
+    result = json.loads(response.read())
+    location_ref[coordinates] = result
+print location_ref
