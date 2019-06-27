2f4eb1277fb98723b0578c74305d1d91004e3515
jainvasu631 <vasujain@D20ML-VJAIN.local>
2019-06-27 13:14:01 -0700

    refactored geonames

--- geonames/geonames.py
+++ geonames/geonames.py
@@ -1,22 +1,27 @@
 import urllib
 import json
 import random
+from parse_coordinates import openCoordinates,parseCoordinates
 geonames_service = {
     'findNearby':"http://api.geonames.org/findNearbyJSON?",
     'findNearbyPlaceName':"http://api.geonames.org/findNearbyPlaceNameJSON?",
     'findNearbyWikipedia':"http://api.geonames.org/findNearbyWikipediaJSON?"
     }
-location_ref = {}
-for i in range(1):
-    lat = str(random.uniform(32,42))
-    lng = str(random.uniform(-124,-117))
-    coordinates = (lat,lng)
+coordinates = openCoordinates("coordinates")
+def generateRandomCoordinates(north=90,south=-90,east=180,west=-180, N=10):
+    coordinates = [{'lat':random.uniform(north,south),'lng':random.uniform(west,east)} for i in range(N)]
+    return coordinates
+    
+def getGeonameData(coordinate = {'lat':0,'lng':0}, service = 'findNearby'):    
     username = "jainvasu631"
     # http://api.geonames.org/findNearbyJSON?lat=47.3&lng=19&username=jainvasu631
     url_base = geonames_service['findNearbyWikipedia']
-    url_params = "lat="+lat+"&lng="+lng+"&username="+username
+    url_params = "lat="+coordinate['lat']+"&lng="+coordinate['lng']+"&username="+username
     url = url_base + url_params
     response = urllib.urlopen(url)
-    result = json.loads(response.read())
-    location_ref[coordinates] = result
-print location_ref
+    geoname_data = json.loads(response.read())
+    return geoname_data
+
+def getGeonamesData(coordinates,service = 'findNearby' ):
+    geoname_data = [getGeonameData(coordinate,service) for coordinate in coordinates]
+    return geoname_data
