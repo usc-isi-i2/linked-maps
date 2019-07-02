0e72f1e1b2c44dc50151bcfa7e09c930ec8bedb7
jainvasu631 <vasujain@D20ML-VJAIN.local>
2019-07-01 17:53:57 -0700

    relation for route metadata  and added printOSM

--- geonames/osm.py
+++ geonames/osm.py
@@ -2,7 +2,7 @@ import urllib
 import json
 import random
 from parse_coordinates import openGeom,parseCoordinates
-def getOSMData(coordinates = [{'lat':0,'lng':0}], key = '"railway"'):    
+def getOSMData(coordinates = [{'lat':0,'lng':0}], key = '"route"="railway"'):    
     lat = [coord['lat'] for coord in coordinates]
     lng = [coord['lng'] for coord in coordinates]
 
@@ -10,12 +10,16 @@ def getOSMData(coordinates = [{'lat':0,'lng':0}], key = '"railway"'):
     url_base = "http://overpass-api.de/api/interpreter?data=[out:json];"
     query_key = key
     query_box = {'s':min(lat),'n':max(lat),'w':min(lng),'e':max(lng)}
-    query = "way[{key}]({s},{w},{n},{e});out body;".format(key=query_key,s=query_box['s'],n=query_box['n'],w=query_box['w'],e=query_box['e'])
+    query = "relation[{key}]({s},{w},{n},{e});out body;".format(key=query_key,s=query_box['s'],n=query_box['n'],w=query_box['w'],e=query_box['e'])
     url = url_base + query
     response = urllib.urlopen(url)
-    geoname_data = json.loads(response.read())
-    print geoname_data
-    return geoname_data
+    json_data = response.read()
+    osm_data = json.loads(json_data)
+    return osm_data['elements']
+
+def printOSMData(osm_Data):
+    for route in osm_Data:
+        print (json.dumps(route['tags']))
 
 coordinates = (openGeom("geom.csv")[1])['wkt']
-data = getOSMData(coordinates)
+printOSMData(getOSMData(coordinates))
