9e3594dde1b5b9796c3d0f6400ab10f87f8b693e
jainvasu631 <vasujain@D20ML-VJAIN.local>
2019-07-02 14:55:41 -0700

    moved osm testing to test.py

--- geonames/osm.py
+++ geonames/osm.py
@@ -1,12 +1,11 @@
 import urllib
 import json
-import random
-from parse_coordinates import openGeom,parseCoordinates
-def getOSMData(coordinates = [{'lat':0,'lng':0}], key = '"route"="railway"'):    
+def getOSMData(coordinates = [{'lat':0,'lng':0}], key = '"route"="railway"', buffer = 0.01):    
     lat = [coord['lat'] for coord in coordinates]
     lng = [coord['lng'] for coord in coordinates]
-
-    #http://overpass-api.de/api/interpreter?data=[out:json];way["railway"](41.5429254269,-122.000388261,41.7613897871,-121.688332858);out body;
+	start_box = {'s':lat[0]-buffer,'n':lat[0]+buffer,'w':lng[0]-buffer,'e':lng[0]+buffer}
+    end_box = {'s':lat[-1]-buffer,'n':lat[-1]+buffer,'w':lng[-1]-buffer,'e':lng[-1]+buffer}
+	#http://overpass-api.de/api/interpreter?data=[out:json];way["railway"](41.5429254269,-122.000388261,41.7613897871,-121.688332858);out body;
     url_base = "http://overpass-api.de/api/interpreter?data=[out:json];"
     query_key = key
     query_box = {'s':min(lat),'n':max(lat),'w':min(lng),'e':max(lng)}
@@ -21,5 +20,3 @@ def printOSMData(osm_Data):
     for route in osm_Data:
         print (json.dumps(route['tags']))
 
-coordinates = (openGeom("geom.csv")[1])['wkt']
-printOSMData(getOSMData(coordinates))
