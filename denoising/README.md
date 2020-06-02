4411b81abf9dcc4f29041b60c78a592e4f972874
Pratulya Bubna <pratulyabubna@outlook.com>
2020-06-01 18:54:26 -0700

    Add README

---
+++ denoising/README.md
@@ -0,0 +1,3 @@
+# <center>Denoising</center>
+## Unsupervised removal of false positives from maps (vector data) using clustering analysis
+<hr>
---
+++ denoising/__init__.py
@@ -0,0 +1,4 @@
+from .agglomerative import AgglomerativeDenoiser
+from .dbscan import DBSCANDenoiser
+
+__all__ = ['AgglomerativeDenoiser', 'DBSCANDenoiser']
