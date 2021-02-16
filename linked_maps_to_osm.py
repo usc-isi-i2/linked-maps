a55a5ea070cd3133fa249e9d6d924cea33aedba5
Basel Shbita <basel921@gmail.com>
2021-02-16 00:42:35 -0800

    support OSM lookup for MULTIPOLYGON type (wetland data)

--- linked_maps_to_osm.py
+++ linked_maps_to_osm.py
@@ -4,19 +4,23 @@ from argparse import ArgumentParser
 from collections import OrderedDict
 from json import loads, dumps
 from os.path import basename
-from random import randint
+from random import uniform, choice
 from time import time, sleep
 from re import sub as re_sub
 from requests import get as get_request
+from shapely import wkt
+from shapely.geometry import Point
+
 
 # --- OSM API -----------------------------------------------------------------
 
 def get_openstreetmap_member_uri(osm_type, osm_id):
-    ''' Get OSM member LGD URI '''
+    ''' Get OSM URI '''
 
-    return "http://linkedgeodata.org/triplify/" + str(osm_type) + str(osm_id)
+    return "https://www.openstreetmap.org/" + str(osm_type) + "/" + str(osm_id)
 
-def query_osm_results(query_box = {'s':0,'n':0,'w':0,'e':0}, filter_tag_or_tagval = ''):
+
+def query_osm_results(query_box={'s': 0, 'n': 0, 'w': 0, 'e': 0}, filter_tag_or_tagval=''):
     ''' Query OSM 'relation' data elements in a given bounding box.
     'relation' elements are used to organize multiple nodes or ways into a larger whole.
     
@@ -24,7 +28,8 @@ def query_osm_results(query_box = {'s':0,'n':0,'w':0,'e':0}, filter_tag_or_tagva
     i.e. 
     http://overpass-api.de/api/interpreter?data=[out:json];node(41.5,-122.0,41.7,-121.6);%3C;out%20meta; '''
 
-    query = "node({s},{w},{n},{e});<;out meta;".format(s=query_box['s'],n=query_box['n'],w=query_box['w'],e=query_box['e'])
+    query = "node({s},{w},{n},{e});<;out meta;".format(s=query_box['s'], n=query_box['n'],
+                                                       w=query_box['w'], e=query_box['e'])
     url_base = "http://overpass-api.de/api/interpreter?data=[out:json];"
     ''' This call includes:
         - all nodes in the bounding box,
@@ -40,51 +45,86 @@ def query_osm_results(query_box = {'s':0,'n':0,'w':0,'e':0}, filter_tag_or_tagva
             filtered_set = list()
             for itm in osm_data['elements']:
                 if 'tags' in itm:
-                    # print("itm['tags'] = " + str(itm['tags']))
                     if filter_tag_or_tagval in itm['tags']:
                         filtered_set.append(itm)
                     else:
                         for itag in itm['tags']:
-                            # print('itag: ' + str(itag) + " = " + str(itm['tags'][itag]))
                             if itm['tags'][itag] == filter_tag_or_tagval:
                                 filtered_set.append(itm)
             return filtered_set
         return osm_data['elements']
-    except ValueError as _:
-        print('ValueError for query_box' + str(query_box))
+    except ValueError as e:
+        print('ValueError for query_box' + str(query_box) + " | " + str(e))
         return []
 
-def create_bounding_box(coordinates = [{'lat':0,'lng':0}], index = None, buffer = 0.001):
+
+def create_bounding_box__multiline(coordinates, random=False, buffer=0.001):
     ''' Create a bounding box given a set of latitude and longitude coordinates. '''
 
-    if index is None:
+    if not random:
         lat = [coord['lat'] for coord in coordinates]
         lng = [coord['lng'] for coord in coordinates]
-        bbox = {'s':min(lat),'n':max(lat),'w':min(lng),'e':max(lng)}
+        bbox = {'s': min(lat), 'n': max(lat), 'w': min(lng), 'e': max(lng)}
     else:
-        lat = coordinates[index]['lat']
-        lng = coordinates[index]['lng']
-        bbox = {'s':lat-buffer,'n':lat+buffer,'w':lng-buffer,'e':lng+buffer}
+        random_choice = choice(coordinates)
+        lat = random_choice['lat']
+        lng = random_choice['lng']
+        bbox = {'s': lat-buffer, 'n': lat+buffer, 'w': lng-buffer, 'e': lng+buffer}
     return bbox
 
-def get_openstreetmap_data(coordinates = [{'lat':0,'lng':0}], qkey='', samples=30):
-    ''' Get OSM metadata in a given bounding box '''
 
+def get_random_point_in_polygon(poly):
+    ''' Get random point in Polygon '''
+    minx, miny, maxx, maxy = poly.bounds
+    while True:
+        p = Point(uniform(minx, maxx), uniform(miny, maxy))
+        if poly.contains(p):
+            return p
+
+
+def create_bounding_box__multipolygon(coordinates, random=False, buffer=0.001):
+    ''' Create a bounding box given a MULTIPOLYGON shapely object. '''
+
+    full_poly = wkt.loads(coordinates)
+    if not random:
+        minx, miny, maxx, maxy = full_poly.bounds
+        bbox = {'s': miny, 'n': maxy, 'w': minx, 'e': maxx}
+    else:
+        # chose one polygon randomly
+        random_poly = choice(list(full_poly))
+        try:
+            random_choice = get_random_point_in_polygon(random_poly)
+        except Exception as e:
+            print("exception in create_bounding_box__multipolygon: " + str(e))
+            random_choice = random_poly.representative_point()
+            pass
+        lat = random_choice.y
+        lng = random_choice.x
+        bbox = {'s': lat-buffer, 'n': lat+buffer, 'w': lng-buffer, 'e': lng+buffer}
+    return bbox
+
+
+def get_openstreetmap_data(coordinates, qkey='', samples=10):
+    ''' Get OSM data and metadata '''
+
+    is_poly = False
     # get bbox that contains all of the coordinates
-    bbox = create_bounding_box(coordinates, None)
-    # print('bbox for the given set of coordinates is: %s' % (str(bbox)))
+    if 'POLYGON' in coordinates:
+        is_poly = True
+        bbox = create_bounding_box__multipolygon(coordinates)
+    else:
+        bbox = create_bounding_box__multiline(coordinates)
+
     bound_data = query_osm_results(bbox, qkey)
     data_dict = {element['id']: element for element in bound_data}
     counts = {element['id']: 1 for element in bound_data}
 
-    num_of_coors_in_wkt = len(coordinates)
-    if num_of_coors_in_wkt < samples:
-        samples = num_of_coors_in_wkt
-    last_index = num_of_coors_in_wkt - 1
-
     for _ in range(samples):
         # sample a bbox by sampling a coordinate from all set
-        sample_bbox = create_bounding_box(coordinates, randint(0, last_index))
+        if is_poly:
+            sample_bbox = create_bounding_box__multipolygon(coordinates, random=True)
+        else:
+            sample_bbox = create_bounding_box__multiline(coordinates, random=True)
         sample_data = query_osm_results(sample_bbox, qkey)
         for element in sample_data:
             if element['id'] not in counts:
@@ -97,15 +137,15 @@ def get_openstreetmap_data(coordinates = [{'lat':0,'lng':0}], qkey='', samples=3
         if identifier in data_dict:
             feature_type = data_dict[identifier]['type']
             feature_identifier = data_dict[identifier]['id']
+            osm_uri = get_openstreetmap_member_uri(feature_type, feature_identifier)
             result.append({'type': feature_type,
                            'id': feature_identifier,
                            'tags': data_dict[identifier]['tags'],
                            'count': counts[identifier],
-                           'lgd_uri': get_openstreetmap_member_uri(feature_type, feature_identifier)})
-        # else:
-            # print('identifier %s was not found in data_dict. skipped...' % (str(identifier)))
+                           'osm_uri': osm_uri})
 
     return result
+
 
 # --- Helpers -----------------------------------------------------------------
 
@@ -116,12 +156,14 @@ def open_geom(fileName):
     geom_data = list()
     for line in lines:
         geom_dict = loads(line)
-        geom_dict['wkt'] = parse_coordinates(geom_dict['wkt'])
+        if 'POLYGON' not in geom_dict['wkt']:  # special parsing for line data (backward compatibility)
+            geom_dict['wkt'] = parse_multiline_coordinates(geom_dict['wkt'])
         geom_data.append(geom_dict)
     return geom_data
 
-def parse_coordinates(line):
-    ''' Parse the WKT literals into coordinates '''
+
+def parse_multiline_coordinates(line):
+    ''' Parse the WKT literals into coordinates (multi-line case) '''
 
     coordinates = line.split(',')
     for i in range(len(coordinates)):
@@ -133,17 +175,20 @@ def parse_coordinates(line):
         coordinates[i] = coordinate
     return coordinates
 
-def get_linkedgeodata_uris(segment, filter_key=''):
+
+def get_osm_uris(segment, filter_key=''):
     ''' Get OSM instances info with their LinkedGeoData URIs '''
 
-    lgd_uris = list()
-    osm_lgd_data = get_openstreetmap_data(segment['wkt'], qkey=filter_key)
-    for itm in osm_lgd_data:
-        print('lgd_uri=%s, counts=%d' % (itm["lgd_uri"], itm["count"]))
-        if itm["count"] > 1: # remove this statement for baseline results
-            lgd_uris.append(itm['lgd_uri'])
+    osm_uris = list()
+    osm_data = get_openstreetmap_data(segment['wkt'], qkey=filter_key)
+    for itm in osm_data:
+        print('osm_uri=%s, counts=%d' % (itm["osm_uri"], itm["count"]))
+        if itm["count"] > 1:  # remove this statement for baseline results
+            osm_uris.append(itm['osm_uri'])
+        #osm_uris.append(itm['osm_uri'])
     
-    return lgd_uris
+    return osm_uris
+
 
 # --- entrypoint --------------------------------------------------------------
 
@@ -157,21 +202,27 @@ def main():
     if args.geometry_file:
         segments = open_geom(args.geometry_file)
         print('there are %d segments (collections of features/vectors) in input file' % (len(segments)))
-        outfile = args.geometry_file.replace('.jl', '.lgd.jl')
+        outfile = args.geometry_file.replace('.jl', '.osm.jl')
         with open(outfile, 'w') as write_file:
             for seg in segments:
-                print("generating LGD URIs for gid %s (has %d coordinate-tuples)" % \
-                      (str(seg['gid']), len(seg['wkt'])))
+                if 'POLYGON' in seg['wkt']:
+                    tot_coor_tuples = 0
+                    for polygon in list(wkt.loads(seg['wkt'])):
+                        tot_coor_tuples += len(polygon.exterior.coords)
+                else:
+                    tot_coor_tuples = len(seg['wkt'])
+                print("generating OSM URIs for gid %s (has %d coordinate-tuples)" % (str(seg['gid']), tot_coor_tuples))
                 line_dict = OrderedDict()
                 line_dict['gid'] = seg['gid']
-                list_of_uris = get_linkedgeodata_uris(seg, args.filtering_key)
-                print("found %d LGD URIs for gid %s" % (len(list_of_uris), str(seg['gid'])))
-                line_dict['lgd_uris'] = list_of_uris
+                list_of_uris = get_osm_uris(seg, args.filtering_key)
+                print("found %d OSM URIs for gid %s" % (len(list_of_uris), str(seg['gid'])))
+                line_dict['osm_uris'] = list_of_uris
                 write_file.write(dumps(line_dict) + '\n')
         print('Exported segments info to file %s' % (outfile))
     else:
         print('Input file was not provided.')
         exit(1)
+
 
 if __name__ == '__main__':
     main()
